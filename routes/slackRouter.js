9b99cc6f80cd54bf45765d961c5e58068dddcec1
Brigadesh Chandrasekar <bchandrasekar@salesforce.com>
2021-10-19 15:50:38 -0500

    post into slack the request body

--- routes/slackRouter.js
+++ routes/slackRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const config = require('config');
 const router = express.Router();
+const { app } = require('./../lib/slackAuth');
 
 const sampledata = [
     { id: 1, name: 'Thor' },
@@ -13,8 +15,21 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log(req.body);
+    postMessage(req.body, res);
     res.send('OK');
 });
 
+const postMessage = async (messagebody, res) => {
+    try {
+        const result = await app.client.chat.postMessage({
+            token: config.get('slackBotCredentials.botToken'),
+            channel: config.get('channelId'),
+            text: messagebody
+        });
+    }
+    catch (error) {
+        console.error(error);
+    }
+};
 
 module.exports = router;
